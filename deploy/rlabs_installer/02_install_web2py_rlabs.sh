313b65b431bc48f6c037b131bba0db4b6f729c33
David Fuertes <dfuertes@unizar.es>
2020-08-06 14:05:43 +0200

    commit que recoge cambios tb en instalador

--- deploy/rlabs_installer/02_install_web2py_rlabs.sh
+++ deploy/rlabs_installer/02_install_web2py_rlabs.sh
@@ -65,7 +65,7 @@ echo "|    Installing OPENRLABS WEB2PY APP       |"
 echo "|                                          |"
 echo "+------------------------------------------+"
 
-#systemctl stop uwsgi.service
+systemctl stop uwsgi.service
 
 tar xf packages/web2py_rlabs.tar.gz -C packages/
 rm -fr $wsp_dir
@@ -73,9 +73,9 @@ mv ./packages/web2py_rlabs $w2p_dir
 
 
 echo "SERVER_OPENGNSYS=$server_opengnsys" > $w2p_dir/setup_init.cfg
-echo "POP3_SERVER=$pop3_server" >> $w2p_dir/setup_init.cfg
 echo "AUTH_METHOD=$auth_method" >> $w2p_dir/setup_init.cfg
 if [ $auth_method == "pop" ]; then
+	echo "POP3_SERVER=$pop3_server" >> $w2p_dir/setup_init.cfg
 	echo "PORT_POP3_SERVER=$port_pop3" >> $w2p_dir/setup_init.cfg
 	echo "USE_TLS=$use_tls" >> $w2p_dir/setup_init.cfg
 fi #End if pop
